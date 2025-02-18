b6e1b3830e976c6b6a626401f323e2dbd3895b0c
miomate <mrspec@outlook.com>
2025-02-18 08:49:26 +0100

    filling homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import { Route, Routes } from 'react-router-dom'
-import SignupPage from './pages/SignupPage'
-import Navbar from './components/Navbar'
-import LoginPage from './pages/LoginPage'
-import ProfilePage from './pages/ProfilePage'
-import PrivateRoute from './components/PrivateRoute'
-import AnonymousRoute from './components/AnonymousRoute'
-import AllSkillsPage from './pages/AllSkillsPage'
-import NewSkillPage from './pages/NewSkillPage'
+import { Route, Routes } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import PrivateRoute from "./components/PrivateRoute";
+import AnonymousRoute from "./components/AnonymousRoute";
+import AllSkillsPage from "./pages/AllSkillsPage";
+import NewSkillPage from "./pages/NewSkillPage";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<h1>Home page</h1>} />
+        <Route path="/" element={<h1>Home page</h1>} />
         <Route
-          path='/signup'
+          path="/signup"
           element={
             <AnonymousRoute>
               <SignupPage />
             </AnonymousRoute>
           }
         />
- 
- 
+
         <Route
-          path='/login'
+          path="/login"
           element={
             <AnonymousRoute>
               <LoginPage />
@@ -33,7 +32,7 @@ function App() {
           }
         />
         <Route
-          path='/profile'
+          path="/profile"
           element={
             <PrivateRoute>
               <ProfilePage />
@@ -41,7 +40,7 @@ function App() {
           }
         />
         <Route
-          path='/skills/new'
+          path="/skills/new"
           element={
             <PrivateRoute>
               <NewSkillPage />
@@ -49,23 +48,37 @@ function App() {
           }
         />
 
-        <Route path='/skills' element={<AllSkillsPage />} />
+        <Route path="/skills" element={<AllSkillsPage />} />
 
-        <Route path='*' element={<h1>404 Page</h1>} />
+        <Route path="*" element={<h1>404 Page</h1>} />
       </Routes>
-      <div class="container">
-        <h1>Welcome to Skill Finder</h1>
-        <p>Skill Finder is a platform that helps people connect with others based on their skills.</p>
-        <h2>How It Works</h2>
-        <p><strong>1. Sign Up / Log In:</strong> Create an account to add and search for skills.</p>
-        <p><strong>2. Add a Skill:</strong> Share your skills by adding them along with your city.</p>
-        <p><strong>3. Search for Skills:</strong> Find people with specific skills in your city.</p>
-        <p><strong>4. Edit or Delete Skills:</strong> Manage your skills easily from your profile.</p>
-        <h2>Start Connecting Today!</h2>
-        <p>Join Skill Finder and connect with talented individuals near you.</p>
-    </div>
+
+      <h1>Welcome to Skill Finder</h1>
+      <p>
+        Skill Finder is a platform that helps people connect with others based
+        on their skills.
+      </p>
+      <h2>How It Works</h2>
+      <p>
+        <strong>1. Sign Up / Log In:</strong> Create an account to add and
+        search for skills.
+      </p>
+      <p>
+        <strong>2. Add a Skill:</strong> Share your skills by adding them along
+        with your city.
+      </p>
+      <p>
+        <strong>3. Search for Skills:</strong> Find people with specific skills
+        in your city.
+      </p>
+      <p>
+        <strong>4. Edit or Delete Skills:</strong> Manage your skills easily
+        from your profile.
+      </p>
+      <h2>Start Connecting Today!</h2>
+      <p>Join Skill Finder and connect with talented individuals near you.</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
