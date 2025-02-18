42b7ed99389d1f9c1fc45eae53048467e565d739
miomate <mrspec@outlook.com>
2025-02-18 08:48:01 +0100

    filling homepage

--- src/App.jsx
+++ src/App.jsx
@@ -23,17 +23,7 @@ function App() {
           }
         />
  
-    <div class="container">
-        <h1>Welcome to Skill Finder</h1>
-        <p>Skill Finder is a platform that helps people connect with others based on their skills.</p>
-        <h2>How It Works</h2>
-        <p><strong>1. Sign Up / Log In:</strong> Create an account to add and search for skills.</p>
-        <p><strong>2. Add a Skill:</strong> Share your skills by adding them along with your city.</p>
-        <p><strong>3. Search for Skills:</strong> Find people with specific skills in your city.</p>
-        <p><strong>4. Edit or Delete Skills:</strong> Manage your skills easily from your profile.</p>
-        <h2>Start Connecting Today!</h2>
-        <p>Join Skill Finder and connect with talented individuals near you.</p>
-    </div>
+ 
         <Route
           path='/login'
           element={
@@ -63,6 +53,17 @@ function App() {
 
         <Route path='*' element={<h1>404 Page</h1>} />
       </Routes>
+      <div class="container">
+        <h1>Welcome to Skill Finder</h1>
+        <p>Skill Finder is a platform that helps people connect with others based on their skills.</p>
+        <h2>How It Works</h2>
+        <p><strong>1. Sign Up / Log In:</strong> Create an account to add and search for skills.</p>
+        <p><strong>2. Add a Skill:</strong> Share your skills by adding them along with your city.</p>
+        <p><strong>3. Search for Skills:</strong> Find people with specific skills in your city.</p>
+        <p><strong>4. Edit or Delete Skills:</strong> Manage your skills easily from your profile.</p>
+        <h2>Start Connecting Today!</h2>
+        <p>Join Skill Finder and connect with talented individuals near you.</p>
+    </div>
     </>
   )
 }
