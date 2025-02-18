40b8395842fd9c30e4642e8403fbca81f8b78282
miomate <mrspec@outlook.com>
2025-02-18 08:53:48 +0100

    filling homepage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import AnonymousRoute from "./components/AnonymousRoute";
 import AllSkillsPage from "./pages/AllSkillsPage";
 import NewSkillPage from "./pages/NewSkillPage";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
@@ -52,31 +53,7 @@ function App() {
 
         <Route path="*" element={<h1>404 Page</h1>} />
       </Routes>
-
-      <h1>Welcome to Skill Finder</h1>
-      <p>
-        Skill Finder is a platform that helps people connect with others based
-        on their skills.
-      </p>
-      <h2>How It Works</h2>
-      <p>
-        <strong>1. Sign Up / Log In:</strong> Create an account to add and
-        search for skills.
-      </p>
-      <p>
-        <strong>2. Add a Skill:</strong> Share your skills by adding them along
-        with your city.
-      </p>
-      <p>
-        <strong>3. Search for Skills:</strong> Find people with specific skills
-        in your city.
-      </p>
-      <p>
-        <strong>4. Edit or Delete Skills:</strong> Manage your skills easily
-        from your profile.
-      </p>
-      <h2>Start Connecting Today!</h2>
-      <p>Join Skill Finder and connect with talented individuals near you.</p>
+      <Homepage />
     </>
   );
 }
